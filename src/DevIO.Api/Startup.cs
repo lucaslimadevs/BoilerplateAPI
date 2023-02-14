3bdf6847546242e92e39ea18e0858692dca37dd1
Lucas Lima <lucaslima.devs@gmail.com>
2023-02-13 21:37:00 -0300

    fix: removing comments

--- src/DevIO.Api/Startup.cs
+++ src/DevIO.Api/Startup.cs
@@ -21,8 +21,7 @@ namespace DevIO.Api
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
             var configuration = new ConfigurationBuilder()
@@ -66,8 +65,7 @@ namespace DevIO.Api
 
             services.ResolveDependecies();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+       
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -78,8 +76,7 @@ namespace DevIO.Api
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseExceptionHandler("/Home/Error");                                
                 app.UseHsts();
             }
 
