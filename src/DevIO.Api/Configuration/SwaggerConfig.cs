a60d8edcabf100a886753b889cf528b03366acbf
lucaslimadevs <lucaslima.devs@gmail.com>
2023-03-16 00:27:16 -0300

    feat: Ajustar documentação do Swagger

---
+++ src/DevIO.Api/Configuration/SwaggerConfig.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Linq;
+
+namespace DevIO.Api.Configuration
+{
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection AddSwaggerConfig(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.OperationFilter<SwaggerDefaultValues>();
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerConfig(this IApplicationBuilder app, IApiVersionDescriptionProvider provider)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                foreach (var description in provider.ApiVersionDescriptions)
+                {
+                    options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+                }
+            });
+
+            return app;
+        }
+    }
+
+    public class ConfigurationSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        readonly IApiVersionDescriptionProvider provider;
+
+        public ConfigurationSwaggerOptions(IApiVersionDescriptionProvider provider) => this.provider = provider;
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var description in provider.ApiVersionDescriptions)
+            {
+                options.SwaggerDoc(description.GroupName, CreatInfoForApiVersion(description));
+            }
+        }
+
+        static OpenApiInfo CreatInfoForApiVersion(ApiVersionDescription description)
+        {
+            var info = new OpenApiInfo()
+            {
+                Title = "API - Boilerplate",
+                Version = description.ApiVersion.ToString(),
+                Description = "Desenvolvida por Lucas Lima - @lucaslimadevs - https://github.com/lucaslimadevs",
+                Contact = new OpenApiContact { Name = "Lucas Lima", Email = "lucaslima.devs@gmail.com" },
+                TermsOfService = new Uri("https://opensource.org/license/mit/"),
+                License = new OpenApiLicense { Name ="MIT", Url = new Uri("https://opensource.org/license/mit/") }
+            };
+
+            if (description.IsDeprecated)
+            {
+                info.Description = "Esta versão está obsoleta!";
+            }
+
+            return info;
+        }
+    }
+
+    public class SwaggerDefaultValues : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var apiDescription = context.ApiDescription;
+
+            operation.Deprecated = apiDescription.IsDeprecated();
+
+            if (operation.Parameters is null) return;
+            
+            foreach (var parameter in operation.Parameters)
+            {
+                var description = apiDescription.ParameterDescriptions.First(p => p.Name == parameter.Name);
+
+                if (parameter.Description is null)
+                {
+                    parameter.Description = description.ModelMetadata?.Description;
+                }
+
+                parameter.Required |= description.IsRequired;
+            }
+
+        }
+    }
+}
--- src/DevIO.Api/Startup.cs
+++ src/DevIO.Api/Startup.cs
@@ -4,11 +4,11 @@ using DevIO.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 using System.IO;
 
 namespace DevIO.Api
@@ -42,11 +42,8 @@ namespace DevIO.Api
                 .AddNewtonsoftJson(options =>
                 options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
             );
-            
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BoilerPlateAPI", Version = "v1" });
-            });
+
+            services.AddSwaggerConfig();
 
             services.AddApiVersioning(options => 
             {
@@ -88,14 +85,12 @@ namespace DevIO.Api
             services.ResolveDependecies();
         }
        
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider provider)
         {
             if (env.IsDevelopment())
             {
                 app.UseCors("Development");
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MinhaAPICore v1"));
+                app.UseDeveloperExceptionPage();                
             }
             else
             {
@@ -116,6 +111,8 @@ namespace DevIO.Api
             app.UseAuthentication();
             
             app.UseAuthorization();
+
+            app.UseSwaggerConfig(provider);
 
             app.UseEndpoints(endpoints =>
             {
